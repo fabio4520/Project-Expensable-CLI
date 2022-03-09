4efac459316c26f5000ca569b0d0fc3887b970e4
Gustavo Leiva <tavodrag@gmail.com>
2022-03-09 18:44:02 -0500

    Welcome & Goodbye

--- expensable.rb
+++ expensable.rb
@@ -1,1 +1,11 @@
 # Start here. Happy coding!
+def welcome
+  print "#" * 36 + "\n" + "#     Welcome to Expensable        #" + "\n" + "#" * 36 + "\n"
+end
+
+def goodbye
+  print "#" * 36 + "\n" + "#   Thanks for using Expensable    #" + "\n" + "#" * 36 + "\n"
+end
+
+welcome
+goodbye
