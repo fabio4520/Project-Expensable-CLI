869aa0d8a7d4bb8e6de3509a1b525643bf01cbc2
Carlos Basurto <carlosxd753@gmail.com>
2022-03-10 10:26:57 -0500

    Start

--- expensable.rb
+++ expensable.rb
@@ -1,11 +1,31 @@
 # Start here. Happy coding!
 def welcome
-  print "#" * 36 + "\n" + "#     Welcome to Expensable        #" + "\n" + "#" * 36 + "\n"
+  print "#" * 36 + "\n" + "#       Welcome to Expensable      #" + "\n" + "#" * 36 + "\n"
+  welcome_options = ["login", "create_user", "exit"]
+  puts welcome_options.join(" | ")
 end
 
+def case_options
+  option = ""
+  # option = gets.chomp
+  until option == "exit"
+    print "> "
+    option = gets.chomp
+    case option
+    when "login"
+      puts "login"
+    when "create_user"
+      puts "create_user"
+    when "exit"
+      goodbye
+    else
+      puts "Invalid option"
+    end
+  end
+end
 def goodbye
-  print "#" * 36 + "\n" + "#   Thanks for using Expensable    #" + "\n" + "#" * 36 + "\n"
+  print "#" * 36 + "\n" + "#    Thanks for using Expensable   #" + "\n" + "#" * 36 + "\n"
 end
 
 welcome
-goodbye
+case_options
